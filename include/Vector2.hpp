f241dbbc636839909186ca469b63f63b388fc6aa
Darenn Keller <keller.darenn@gmail.com>
2016-10-06 10:26:30 +0200

    Fixed merge script bug.

--- include/Vector2.hpp
+++ include/Vector2.hpp
@@ -1,6 +1,9 @@
 #ifndef VECTOR2_H
 #define VECTOR2_H
 
+#include "truc"
+#include <machin>
+
 class Vector2 {
 public:
   float x, y;
--- scripts/merge.sh
+++ scripts/merge.sh
@@ -3,5 +3,4 @@
 
 find ./include/ -name "*.hpp" -print0 | xargs -0 cat > merged.cpp
 find ./src/ -name "*.cpp" -print0 | xargs -0 cat >> merged.cpp
-sed '/#include/d' merged.cpp
-sed '/# include/d' merged.cpp
+sed '/#include "*"/d' merged.cpp
